c8f7b7ecbe183ce6dceb037415297c5434c8c5b2
shourya201116-max <shourya201116@gmail.com>
2026-08-26 21:07:31 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -20,3 +20,13 @@ export const app = isFirebaseConfigured ? initializeApp(firebaseConfig) : null;
 export const db = app ? getFirestore(app) : null;
 
 export { firebaseConfig };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBl4hIn8hRRDiQi2NIY1SMQ3W6ZE2EBLeU",
+  authDomain: "sam-library.firebaseapp.com",
+  projectId: "sam-library",
+  storageBucket: "sam-library.firebasestorage.app",
+  messagingSenderId: "423256204857",
+  appId: "1:423256204857:web:6b246e1a0207b2d0058fe8",
+  measurementId: "G-0TC0YX3779"
+};
